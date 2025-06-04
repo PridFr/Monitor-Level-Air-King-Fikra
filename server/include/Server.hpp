b7de16d0801a2b291f2a27a3dd0590b1d7b84ebd
PridFr <ahmad.farrij@ui.ac.id>
2025-06-04 10:57:36 +0700

    kembali pakai winsock untuk pembuatan server

--- server/include/Server.hpp
+++ server/include/Server.hpp
@@ -1,26 +1,84 @@
 #ifndef SERVER_H
 #define SERVER_H
-#define ASIO_STANDALONE
-#include <asio.hpp>
-//#include <config.h>
-#include <iostream>
+
 #include <memory>
+#include <queue>
+#include <functional>
 #include <thread>
-#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <sstream>
+#include "../shared/include/Tank.h"
 
-
-class Server {
-public:
-    Server(asio::io_context& io_context, unsigned short port);
-    void run(unsigned int thread_count = std::thread::hardware_concurrency());
-
+// Forward declarations
+class ThreadPool;
+class Session;
+//Class thread pool
+class ThreadPool {
 private:
-    void start_accept();
-    void start_session(std::shared_ptr<asio::ip::tcp::socket> socket);
+    std::vector<std::thread> workers; //deklarasi worker
+    std::queue<std::function<void()>> tasks; //antrian tugas
+    std::mutex queue_mutex;
+    std::condition_variable condition;
+    bool stop;
 
-    asio::io_context& io_context_;
-    asio::ip::tcp::acceptor acceptor_;
+public:
+    explicit ThreadPool(size_t threads);
+    ~ThreadPool();
+    
+    template<class F>
+    void enqueue(F&& f) { //fungsi menambahkan tugas baru ke thread pool
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            tasks.emplace(std::forward<F>(f));
+        }
+        condition.notify_one();
+    }
 };
 
+class Session {
+private:
+    SOCKET client_socket;
+    static const int BUFFER_SIZE = 1024;
+    char buffer[BUFFER_SIZE];
+    std::shared_ptr<Server> server_ptr;
+
+    // Helper functions untuk parsing dan handling data
+    TankData parse_data(const std::string& data);
+    void handle_tank_data(const TankData& data);
+
+public:
+    Session(SOCKET socket, std::shared_ptr<Server> server);
+    ~Session();
+    void handle_client();
+};
+
+class Server : public std::enable_shared_from_this<Server> {
+private:
+    WSADATA wsaData;
+    SOCKET server_soc;
+    struct addrinfo* result;
+    struct addrinfo hints;
+    int result_idx;
+    std::unique_ptr<ThreadPool> pool;
+
+    // Collection untuk menyimpan data tank
+    std::vector<TankData> tank_data_collection;
+    mutable std::mutex data_mutex;  // Untuk thread-safe access ke collection
+
+public:
+    Server();
+    ~Server();
+    void socketStart();
+    void serverListen();
+    void acceptClient();
+
+    
+    // Fungsi untuk akses data
+    void add_tank_data(const TankData& data);
+    std::vector<TankData> get_tank_data() const;
+};
 
 #endif
